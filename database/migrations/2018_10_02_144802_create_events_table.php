f694ac389160ef94b19302736f142ebdc7706dbc
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-10-02 18:00:28 +0300

    feat:create database migrations

---
+++ database/migrations/2018_10_02_144802_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image');
+            $table->string('title');
+            $table->mediumText('description');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->string('location');
+            $table->string('posted_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
