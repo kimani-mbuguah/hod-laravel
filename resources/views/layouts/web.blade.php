f4b52ac6c733471a64fc2031ae54d6ebd0e019f2
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-10-14 18:21:03 +0300

    use javascript to get dynamic copyright year

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -221,7 +221,7 @@
         </footer><!-- FOOTER -->
         <div class="bottom-footer">
                 <div class="container">
-                        <p>©2018 <a href="#" title=""></a>The House of Destiny Church</a>All rights reserved. Powered by <a href="#" title="">Kim</a></p>
+                        <p>&copy; <script>document.write(new Date().getFullYear());</script> <a href="#" title=""></a>The House of Destiny Church</a>All rights reserved. Powered by <a href="#" title="">Kim</a></p>
                     </div>
         </div><!-- BOTTOM FOOTER STRIP -->
         <div class="sidepanel">
